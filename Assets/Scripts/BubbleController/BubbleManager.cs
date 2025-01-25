eda216306d7cfdadd9d426d63603c144667b166f
Lozmep <lozmep.adm@gmail.com>
2025-01-25 14:33:51 -0500

    Modify bubble and letter controller

--- Assets/Scripts/BubbleController/BubbleManager.cs
+++ Assets/Scripts/BubbleController/BubbleManager.cs
@@ -1,18 +1,46 @@
+using SignalSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BubbleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Bubble Variables")]
+    public bool isActive;
+
+    [Header("Game components")]
+    public LetterController letterController;
+    public LetterObject currentLetter;
+    // spawner
+
+    private void Start()
     {
         
+    }
+    public void SelectLevel(int levelId)
+    {
+        letterController.SetLevelInfo(levelId);
+        isActive = true;
+        //mas cosas
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator StartLevel() 
     {
-        
+        // las burbujas se spawnean aleatoriamente.
+        while (isActive) {
+            letterController.ShuffleLettersInLevel();
+            currentLetter = letterController.levelInfo.letters[0];
+            // FUNCION SPAWNER (LetterObject letter)
+            letterController.SubstractPoolLetter(currentLetter.letter);
+            yield return new WaitForSeconds(1f);
+            if (letterController.GetLetterCount() == 0)
+            {
+                isActive = false;
+            }
+        }           
     }
+
+
+
+
 }
