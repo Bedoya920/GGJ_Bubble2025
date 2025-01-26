9a5896f167927268e1ce69df29e973b1b61656f6
Valuv <valuv027@gmail.com>
2025-01-25 21:29:01 -0800

    Contador de colores

--- Assets/Scripts/LevelController/LevelManager.cs
+++ Assets/Scripts/LevelController/LevelManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
@@ -12,7 +13,10 @@ public class LevelManager : MonoBehaviour
     public BubbleManager bubbleManager;
     public LetterController letterController;
     public BubbleSpawn bubbleSpawn;
-    
+
+    public Text timerText; 
+    public List<Color> colors; 
+    public int totalTime = 10; 
     private void Awake()
     {
         levelID = PlayerPrefs.GetInt("levelId");
@@ -24,6 +28,7 @@ public class LevelManager : MonoBehaviour
         Debug.Log("levelId" + levelID);
         bubbleManager.SelectLevel(levelID);
         StartCoroutine(bubbleManager.StartLevel());
+        StartCoroutine(StartTimer());
     }
 
     public void NextScene()
@@ -63,6 +68,47 @@ public class LevelManager : MonoBehaviour
         Time.timeScale = 1f;
     }
 
+    IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(0.5f);
 
+        int timeLeft = totalTime;
+
+        while (timeLeft >= 0)
+        {
+            
+            string timerString = timeLeft.ToString("0");
+
+            
+            string coloredText = "";
+            for (int i = 0; i < timerString.Length; i++)
+            {
+                if (i == timerString.Length - 1) // Solo el último dígito (el de la derecha)
+                {
+                    // Asigna un color de la lista al dígito que cambia
+                    Color color = colors[timeLeft % colors.Count]; // Usa el tiempo restante para elegir el color
+                    coloredText += $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{timerString[i]}</color>";
+                }
+                else
+                {
+                    // El otro dígito permanece con el color predeterminado (blanco o el que tenga el texto)
+                    coloredText += timerString[i];
+                }
+            }
+
+            
+            timerText.text = coloredText;
+
+            
+            yield return new WaitForSeconds(0.5f);
+
+            
+            timeLeft--;
+        }
+
+        yield return new WaitForSeconds(1f);
+        //NextScene();
+
+    }
 
 }
