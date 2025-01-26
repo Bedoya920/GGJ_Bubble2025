e5c20dc730290225b39a9e1cbc54456c62fe19a8
Samuel Bedoya <samy040902@gmail.com>
2025-01-26 11:21:58 -0500

    GameOver listo

--- Assets/Scripts/HUD/AnimGameOver.cs
+++ Assets/Scripts/HUD/AnimGameOver.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class AnimGameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void CallPause()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        LevelManager.instance.PauseGame();
     }
 }
