558475178ae5cdb0e0483286ee89633796d9d154
Lozmep <lozmep.adm@gmail.com>
2025-01-26 09:44:12 -0500

    Adjust score formula

--- Assets/Scripts/HUD/ScoreManager.cs
+++ Assets/Scripts/HUD/ScoreManager.cs
@@ -13,7 +13,7 @@ public class ScoreManager : MonoBehaviour
     public void IncreaseScore(int streak)
     {
         float multiplier = Mathf.Min(1f + (streak * 0.1f), 3f);
-        int pointsEarnet = Mathf.RoundToInt(basePoints * multiplier);
+        int pointsEarnet = Mathf.RoundToInt(streak > 0 ? basePoints * multiplier : 0);
         score += pointsEarnet;
     }
     public void SetAccuracy(int correctCounter, int incorrectCounter)
